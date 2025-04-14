45cb6192d58b42baba4e2bc99355e077ad6fd788
Chris Jaehnen <cjaehnen@users.noreply.github.com>
2025-04-14 19:01:42 -0400

    Perform automatic project sync changes

--- buildSrc/src/main/kotlin/io/opengood/project/sync/task/SyncGitCommit.kt
+++ buildSrc/src/main/kotlin/io/opengood/project/sync/task/SyncGitCommit.kt
@@ -49,7 +49,7 @@ open class SyncGitCommit : BaseTask() {
                         val status = git.status()
 
                         if (status.isNotBlank()) {
-                            printInfo("Determining current Git status:")
+                            printInfo("Determining current Git status...")
                             printInfo(status)
                             printDone()
 
